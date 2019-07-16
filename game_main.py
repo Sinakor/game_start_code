d25a4c477a0b064c7e520544af1b1fa9744b6581
John Fittro <J.Fittro@gmail.com>
2019-07-15 21:10:38 -0700

    made a room

--- game_main.py
+++ game_main.py
@@ -1,20 +1,15 @@
 import room, item, character
 
 hero_player = character.Player()
-main_room = room.Room([hero_player], 10, 10)
+main_room = room.Room([hero_player], 50, 50)
 
 keep_playing = True
 while keep_playing:
 	print(repr(main_room))
+	# print(main_room.coord_dict)
 	inkey = input("Next command: ")
 	if inkey[0] in ['w', 'W']:
 		new_loc = (hero_player.location[0] - 1, hero_player.location[1])
 		hero_player.location = new_loc
 	elif inkey[0] in ['q', 'Q']:
 		keep_playing = False
-
-
-
-
-
- 
--- room.py
+++ room.py
@@ -1,36 +1,41 @@
 import item
 
+
 class Coord():
-	def __init__(self, in_coord, item=None):
-		self.coord = in_coord
-		self.has = item
+    def __init__(self, in_coord, item=None):
+        self.coord = in_coord
+        self.has = item
 
-	def __str__(self):
-		return str(self.coord)
+    def __str__(self):
+        return str(self.coord)
 
-	def __repr__(self):
-		return str(self.has)
+    def __repr__(self):
+        return str(self.has)
 
-	def place(self, in_item):
-		self.has = in_item
+    def place(self, in_item):
+        self.has = in_item
+
 
 class Room():
-	def __init__(self, characters, width, height):
-		self.characters = characters
-		self.coord_dict = {}
-		self.width = width
-		self.height = height
-		
-		# Fill the room with coordinates that have no item
-		for i in range(height):
-			for j in range(width):
-				self.coord_dict[(i, j)] = Coord((i, j))
+    def __init__(self, characters, width, height):
+        self.characters = characters
+        self.coord_dict = {}
+        self.width = width
+        self.height = height
 
-		# Make a single sample box at 1 down, 5 right
-		single_box = item.Item('box', '☐')
-		self.coord_dict[(1, 5)].place(single_box)
-	
-	def __repr__(self):
-	
-		# FIX THIS REPR
-		return ('map '*10 + '\n') * 10 + ''.join([str(character.location) + '\n' for character in self.characters])
+        # Fill the room with coordinates that have no item
+        for i in range(height):
+            for j in range(width):
+                self.coord_dict[(i, j)] = Coord((i, j))
+
+        # Make a single sample box at 1 down, 5 right
+        single_box = item.Item('box', '☐')
+        self.coord_dict[(1, 5)].place(single_box)
+
+    def __repr__(self):
+        room = []
+        room.append('* '*(self.width+2))
+        for _ in range(self.height):
+            room.append('* ' + '  '*self.width + '*')
+        room.append('* '*(self.width+2))
+        return '\n'.join(room)
