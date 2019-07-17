78589676ad5646627a52a92531204cef11062420
Adriana Cantu <adrianajcantu@gmail.com>
2019-07-16 21:02:51 -0700

    "updated directions and game main"

--- game_main.py
+++ game_main.py
@@ -7,23 +7,24 @@ keep_playing = True
 while keep_playing:
 	print(repr(main_room))
 	# print(main_room.coord_dict)
+	main_room.coord_dict[hero_player.location].place(None)
 	inkey = input("Next command: ")
-	if inkey[0] in ['w', 'west', 'l', 'left']:
+	if inkey[0] in ['n', 'north','up','u']:
 		new_loc = (hero_player.location[0] - 1, hero_player.location[1])
 		hero_player.location = new_loc
 		print(repr(new_loc))
-	elif inkey[0] in ['e', 'east', 'r', 'right']:
+	elif inkey[0] in ['s', 'south','down','d']:
 		new_loc = (hero_player.location[0] + 1, hero_player.location[1])
 		hero_player.location = new_loc
 		print(new_loc)
-	elif inkey[0] in ['n', 'north','up','u']:
+	elif inkey[0] in ['e', 'east', 'r', 'right']:
 		new_loc = (hero_player.location[0], hero_player.location[1] + 1)
 		hero_player.location = new_loc
 		print(new_loc)
-
-	elif inkey[0] in ['s', 'south','down','d']:
+	elif inkey[0] in ['w', 'west', 'l', 'left']:
 		new_loc = (hero_player.location[0], hero_player.location[1] - 1)
 		hero_player.location = new_loc
 		print(new_loc)
-	#print(repr(main_room))
+	main_room.update()
+#print(repr(main_room))
 # #keep_playing = False
