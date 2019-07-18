32b30f16efa6596ed98ca44ea29fc6f5b95bded7
John Fittro <J.Fittro@gmail.com>
2019-07-17 18:34:09 -0700

    putting the room back in the branch

---
+++ game_main.py
@@ -0,0 +1,30 @@
+import room, item, character
+
+hero_player = character.Player()
+main_room = room.Room([hero_player], 10, 10)
+
+keep_playing = True
+while keep_playing:
+	print(repr(main_room))
+	# print(main_room.coord_dict)
+	main_room.coord_dict[hero_player.location].place(None)
+	inkey = input("Next command: ")
+	if inkey[0] in ['n', 'north','up','u']:
+		new_loc = (hero_player.location[0] - 1, hero_player.location[1])
+		hero_player.location = new_loc
+		print(repr(new_loc))
+	elif inkey[0] in ['s', 'south','down','d']:
+		new_loc = (hero_player.location[0] + 1, hero_player.location[1])
+		hero_player.location = new_loc
+		print(new_loc)
+	elif inkey[0] in ['e', 'east', 'r', 'right']:
+		new_loc = (hero_player.location[0], hero_player.location[1] + 1)
+		hero_player.location = new_loc
+		print(new_loc)
+	elif inkey[0] in ['w', 'west', 'l', 'left']:
+		new_loc = (hero_player.location[0], hero_player.location[1] - 1)
+		hero_player.location = new_loc
+		print(new_loc)
+	main_room.update()
+#print(repr(main_room))
+# #keep_playing = False
